394ad84a0826a9a8507ffc0d74b2de04fc8af4bf
John Swanson <john.d.swanson@gmail.com>
2015-07-25 21:42:22 -0700

    upgrade dependencies
    0.2.10 of tools.namespace added support for cljc files

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject reloaded.repl "0.1.0"
+(defproject org.clojars.jds02006/reloaded.repl "0.1.1"
   :description "REPL functions to support the reloaded workflow"
-  :url "https://github.com/weavejester/reloaded.repl"
+  :url "https://github.com/johnswanson/reloaded.repl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.namespace "0.2.4"]
-                 [com.stuartsierra/component "0.2.1"]])
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [com.stuartsierra/component "0.2.3"]])
